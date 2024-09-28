439be9468bda008d902901985afd55107d4bcd7a
ralburquequecarrillo <ralburquequecarrillo@gmail.com>
2024-09-28 17:59:17 -0500

    Add files via upload

---
+++ vars/myDockerPipeline.groovy
@@ -0,0 +1,20 @@
+def call(Map params){
+    pipeline{
+        stages{
+            stage("Construyendo docker"){
+                script{
+                    sh : myDockerLib.build(rutaArchivoDocker: "${params.rutaArchivoDocker}",
+                                          nombreImagenDocker: "${params.nombreImagenDocker}",
+                                          carpetaDocker: "${params.carpetaDocker}")
+                }
+            }
+
+            stage("Pusheando docker"){
+                script{
+                    sh : myDockerLib.push(nombreImagenDocker: "${params.nombreImagenDocker}")
+                }
+            }
+        }
+
+    }
+}
