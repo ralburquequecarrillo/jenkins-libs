a10f8b9fa4455a2f4911c53871e4c262dc5d5494
ralburquequecarrillo <ralburquequecarrillo@gmail.com>
2024-09-28 18:07:21 -0500

    Update myDockerPipeline.groovy

--- vars/myDockerPipeline.groovy
+++ vars/myDockerPipeline.groovy
@@ -1,22 +1,25 @@
 def call(Map params){
     pipeline{
-        anget any
-        
         stages{
             stage("Construyendo docker"){
-                script{
-                    sh : myDockerLib.build(rutaArchivoDocker: "${params.rutaArchivoDocker}",
-                                          nombreImagenDocker: "${params.nombreImagenDocker}",
-                                          carpetaDocker: "${params.carpetaDocker}")
+                steps{
+                    script{
+                        sh : myDockerLib.build(rutaArchivoDocker: "${params.rutaArchivoDocker}",
+                                            nombreImagenDocker: "${params.nombreImagenDocker}",
+                                            carpetaDocker: "${params.carpetaDocker}")
+                        }
                 }
+                
             }
 
             stage("Pusheando docker"){
-                script{
-                    sh : myDockerLib.push(nombreImagenDocker: "${params.nombreImagenDocker}")
+                steps{
+                    script{
+                        sh : myDockerLib.push(nombreImagenDocker: "${params.nombreImagenDocker}")
+                    }
                 }
+                
             }
         }
-
     }
 }
