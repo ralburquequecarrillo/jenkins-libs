e189bc39869362ebc69dd3d4aefb9f4b3e2afc63
ralburquequecarrillo <ralburquequecarrillo@gmail.com>
2024-09-28 18:44:42 -0500

    Update myDockerLib.groovy

--- vars/myDockerLib.groovy
+++ vars/myDockerLib.groovy
@@ -1,7 +1,7 @@
 def build(Map params){
-    sh: "docker build -f ${params.rutaArchivoDocker} -t ${params.nombreImagenDocker} ${params.carpetaDocker}"
+    sh "docker build -f ${params.rutaArchivoDocker} -t ${params.nombreImagenDocker} ${params.carpetaDocker}"
 }
 
 def push(Map params){
-    sh: "docker push ${params.nombreImagenDocker}"
+    sh "docker push ${params.nombreImagenDocker}"
 }
